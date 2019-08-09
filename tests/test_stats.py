361de63d6bef31ea81a6bbafe8fd22da9adbb99f
paulknewton <paul@topolino.org>
2019-08-09 21:45:47 +0200

    Added test for divide by 0

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -98,6 +98,39 @@ def test_multi_lookup(mock_read_stats_as_text, grammatik):
     assert stats_df.equals(multi_df)
 
 
+@mock.patch("statistics.MinecraftStats._read_stats_as_text")
+def test_zero_deaths(mock_read_stats_as_text):
+    """
+    Test lookup for a user with zero deaths (avoid divide by zero)
+    :param mock_read_stats_as_text: mock stats reading function
+    :param grammatik: fixture data
+    """
+    # test data
+    grammatik_dict = {
+        'Grammatik0': {'Wins': '391', 'Kills': '1259', 'Games': '1069', 'Beds destroyed': '725', 'Deaths': '0'}}
+
+    # in dataframe form
+    df_columns = list(grammatik_dict["Grammatik0"].keys())
+    df_columns.append("K/D")
+    df_data = list(grammatik_dict["Grammatik0"].values())
+    df_data.append(0)
+    grammatik_df = pandas.DataFrame.from_dict({"Grammatik0": df_data}, orient="index", columns=df_columns)
+
+    # sample stats text data (from a file)
+    with open("tests/grammatik_zero_deaths_stats.txt", "r") as text_file:
+        dummy_stats_text = text_file.read()
+
+    # configure mock class to return hard-coded stats text
+    stats_reader = MinecraftStats()
+    mock_read_stats_as_text.return_value = dummy_stats_text
+
+    stats = stats_reader.get_stats("Grammatik0")
+    assert stats == grammatik_dict
+
+    stats_df = stats_reader.get_stats_df("Grammatik0")
+    assert stats_df.equals(grammatik_df)
+
+
 def test_invalid_url(grammatik):
     """
     Test lookup with a non-http protocol in the URL
@@ -131,7 +164,8 @@ def test_invalid_stats(mock_read_stats_as_text, grammatik):
     df_columns = list(grammatik[0][user].keys())
     df_columns.append("K/D")
     df_invalid_data = [None] * len(df_columns)
-    grammatik_invalid_df = pandas.DataFrame.from_dict({"Grammatik": df_invalid_data}, orient="index", columns=df_columns)
+    grammatik_invalid_df = pandas.DataFrame.from_dict({"Grammatik": df_invalid_data}, orient="index",
+                                                      columns=df_columns)
     print(grammatik_invalid_df)
 
     stats_df = stats_reader.get_stats_df(user)
